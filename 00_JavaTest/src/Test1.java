d4a556aa79ebe0e3c40057a52e23fea914dbae02
jiwoolee2 <dlwldn1009@naver.com>
2025-01-20 17:29:34 +0900

    VariableEx3

--- 00_JavaTest/src/Test1.java
+++ 00_JavaTest/src/Test1.java
@@ -1,9 +1,9 @@
 // JS와 주석 동일
 
 // Run 버튼 클릭 시 (단축키 ctrl + f11)
-// 1) 자동으로 컴파일 수행(javac ooo.java)
-// -> .class 파일(byte cod)생성
-// 2) 컴파일 완료 후 .class 파일 실행(java class 파일명)
+/*  1) 자동으로 컴파일 수행(javac ooo.java)
+ 		-> .class 파일(byte cod)생성
+	2) 컴파일 완료 후 .class 파일 실행(java class 파일명)*/
 
 public class Test1 {
 
@@ -13,7 +13,25 @@ public class Test1 {
 		System.out.println("자꾸 그러지마..");
 		System.out.println("이클립스 실행 중");
 		System.out.println("이클립스 실행 중");
+		
+		/* Java API DOC
+		 * : Java에서 제공해주는 코드의 사용법이 설명되어있는 문서
+		 *  
+		 * DOC 확인 방법
+		 * 1) google -> java 21 api 검색
+		 * 
+		 * 2) 궁굼한 코드 클릭 -> shift + f2
+		 * */
+		
 
+		
+		
+		
+		
+		
+		
+		
+		
 	}
-
 }
+
