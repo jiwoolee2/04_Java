36f75da2e787e696ad8c3186ccf6e669c4d91c76
jiwoolee2 <dlwldn1009@naver.com>
2025-01-20 11:25:34 +0900

    java workspace 등록

---
+++ 00_JavaTest/src/Test1.java
@@ -0,0 +1,19 @@
+// JS와 주석 동일
+
+// Run 버튼 클릭 시 (단축키 ctrl + f11)
+// 1) 자동으로 컴파일 수행(javac ooo.java)
+// -> .class 파일(byte cod)생성
+// 2) 컴파일 완료 후 .class 파일 실행(java class 파일명)
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("이클립스 실행 중");
+		System.out.println("자꾸 그러지마..");
+		System.out.println("이클립스 실행 중");
+		System.out.println("이클립스 실행 중");
+
+	}
+
+}
---
+++ JavaTest.java
@@ -0,0 +1,17 @@
+public class JavaTest{
+
+  public static void main(String[] args){
+
+   System.out.println("Hellow world!!"); 
+   System.out.println("Hellow world!!");
+   System.out.println("Hellow world!!");
+   System.out.println("Hellow world!!");
+   System.out.println("Hellow world!!"); 
+   
+   for(int i =1 ; i<5 ; i++){
+	System.out.println(i+"번째 출력");
+     }
+   }
+
+}
+
