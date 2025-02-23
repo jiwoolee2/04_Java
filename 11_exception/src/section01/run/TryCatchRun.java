20f673d06a5c2109612700f4e9fb8f59f56a4263
jiwoolee2 <dlwldn1009@naver.com>
2025-02-23 23:55:04 +0900

    0223

--- 11_exception/src/section01/run/TryCatchRun.java
+++ 11_exception/src/section01/run/TryCatchRun.java
@@ -1,5 +1,11 @@
 package section01.run;
 
+/* 에외처리방법 
+ * 1. try exception  구문을 통해 예외가 발생한 곳에서 처리한다.
+ * 
+ * 2. 예외가 발생한 메서드의 호출부로 예외를 넘겨서 처리한다.
+
+ * */
 import section01.service.TryCatchService;
 
 public class TryCatchRun {
